23f33c68d2d3d355f8b4cc480b39a3822f9820e4
Dean <deanm>
2018-08-07 21:11:11 -0700

    playing around with live reloading

--- bootcamp/App.js
+++ bootcamp/App.js
@@ -10,20 +10,26 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: 'PRESSS Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    greeting: 'Welcome to React Bootcamp!'
+  }
+  updateGreeting = () => {
+    this.setState({
+      greeting: "New Greeting",
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>{this.state.greeting}</Text>
+        <Text style={styles.welcome} onPress={this.updateGreeting}>hahah Change 123 Greeting</Text>
       </View>
     );
   }
@@ -34,16 +40,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#333',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: 'white',
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: 'white',
     marginBottom: 5,
   },
 });
